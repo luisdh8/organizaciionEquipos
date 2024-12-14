e830919cb73e93680145de71bbb1fd81ab53d6e5
Luis Díaz <luisdh.8@outlook.com>
2024-12-13 19:43:29 -0600

    Mi Org
    Clase 5 Modulo "Mi Org"

---
+++ org/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Formulario from './components/Formulario/Formulario';
+import MiOrg from './components/MiOrg';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Formulario />
+      <MiOrg />
+    </div>
+  );
+}
+
+export default App;
---
+++ org/src/components/ListaOpciones/index.js
@@ -0,0 +1,25 @@
+import "./ListaOpciones.css";
+
+const ListaOpciones = () => {
+
+    const equipos = [
+        "Programación",
+        "Front End",
+        "Data Science",
+        "Devops",
+        "UX y Diseño",
+        "Móvil",
+        "Innovación y Gestión"
+    ]
+
+    return <div className="lista-opciones">
+        <label>Equipos</label>
+        <select>
+            {equipos.map((equipo, index) => {
+                return <option key={index}>{equipo}</option>
+            })}
+        </select>
+    </div>
+};
+
+export default ListaOpciones;
